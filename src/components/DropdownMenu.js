827e2c9e060c135b55fdb85e8d0419aa0eaf7fed
brendino500 <brenda.ty@live.com>
2021-09-19 13:36:25 +0100

    data

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -4,9 +4,11 @@ import MenuItem from '@material-ui/core/MenuItem'
 import FormControl from '@material-ui/core/FormControl'
 import Select from '@material-ui/core/Select'
 
+import data from '../data/data.json'
+
 const DropdownMenu = () => {
   const [length, setLength] = useState(0)
-  const numbers = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+  const numberRange = data.password.passwordArr
 
   const handleSelect = (e) => {
     setLength(e.target.value)
@@ -17,7 +19,7 @@ const DropdownMenu = () => {
       <FormControl style={{ width: '100px' }}>
         <InputLabel>Number of Characters</InputLabel>
         <Select value={length} onChange={handleSelect}>
-          {numbers.map((num) => (
+          {numberRange.map((num) => (
             <MenuItem value={num}>{num}</MenuItem>
           ))}
         </Select>
